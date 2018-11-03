0756ba0e6de73dac3352ea24d6178443f4d225b3
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2018-11-03 14:46:33 -0700

    Add Toolbar with Image Picker
    Add a toolbar to the bottom of the View Controller view with a single button to open an image picker. The delegate method for UIImagePickerController has been implemented but does not yet return the selected image to the view for display. Text is printed to the debug console to verify that the delegate methods for selecting an image and dismissing the photo picker are properly implemented.

--- ios-nanodegree-mememe/ViewController.swift
+++ ios-nanodegree-mememe/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBActions
+    @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
+// MARK: - Image Picker Delegate w/ Navigation Controller Delegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("image selected")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("image selection cancelled")
+        dismiss(animated: true, completion: nil)
+    }
+}
