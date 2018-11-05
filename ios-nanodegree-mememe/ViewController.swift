9cbc0e321e1457ac2bf2d0d7c30fdf3ce0e00561
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2018-11-04 16:56:04 -0800

    Adjust View When Keyboard is Visible
    Listen for the show/hide keyboard notifications and adjust the origin of the view if the keyboard will obscure the text field being edited.

--- ios-nanodegree-mememe/ViewController.swift
+++ ios-nanodegree-mememe/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var topTextField: UITextField!
     
+    // MARK: - Properties
+    var selectedTextField: UITextField!
+    
     // MARK: - IBActions
     @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
         let pickerController = UIImagePickerController()
@@ -60,7 +63,52 @@ class ViewController: UIViewController {
         textField.textAlignment = .center
     }
     
-    // MARK: View Controller
+    // MARK: - Manage Keyboard and Keyboard Notifications
+    /**
+     Get the height of the onscreen keyboard.
+     - parameter notification: The *Notification* observed by the Notification Center.
+     - returns: The height of the onscreen keyboard as a *CGFloat*.
+     */
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    /**
+     Reset the origin of the view to zero.
+     - parameter notification: The *Notification* observed by the Notification Center.
+     */
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    /**
+     Adjust the origin of the view to accomodate the height of the onscreen keyboard if is determined that
+     the onscreen keyboard will obscure the selected *UITextField*.
+     - parameter notification: The *Notification* observed by the Notification Center.
+     */
+    @objc func keyboardWillShow(_ notification: Notification) {
+        let keyboardHeight = getKeyboardHeight(notification)
+        
+        if selectedTextField.frame.origin.y > keyboardHeight {
+            view.frame.origin.y = -keyboardHeight
+        }
+    }
+    
+    /// Subscribe to the keyboardWillHide and keyboardWillShow notifications.
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    /// Unsubscribe from the keyboardWillHide and keyboardWillShow notifications.
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    // MARK: - View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -71,7 +119,13 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
 }
 
@@ -100,10 +154,12 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
 // MARK: - Text Field Delegate
 extension ViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
+        selectedTextField = textField
         textField.text = ""
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        selectedTextField = nil
         textField.resignFirstResponder()
         return true
     }
