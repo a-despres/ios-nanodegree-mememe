88970429eaa39d762acede45f57c468242b6b0f6
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2018-11-11 18:32:09 -0800

    Display Memes in Collection View
    Display memes from the shared memes array found in the app delegate in a UICollectionView. Selecting a meme removes the meme from the array and sends it to the view meme view controller where it will either be reinserted into the array if the user returns to the collection view without editing, or will be replaced with an edited version if the user chooses to edit the meme.

--- ios-nanodegree-mememe/View Controllers/SentMemesCollectionViewController.swift
+++ ios-nanodegree-mememe/View Controllers/SentMemesCollectionViewController.swift
@@ -21,6 +21,11 @@ class SentMemesCollectionViewController: UICollectionViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
@@ -39,19 +44,36 @@ class SentMemesCollectionViewController: UICollectionViewController {
 // MARK: - Collection View Controller
 extension SentMemesCollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return appDelegate.memes.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SentMemesCollectionViewCell
         
         // round corners
         cell.layer.cornerRadius = 2.0
         cell.layer.masksToBounds = true
+        
+        // set image
+        cell.memeImage.image = appDelegate.memes[indexPath.row].memeImage
         return cell
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("selected image at \(indexPath)")
+        performSegue(withIdentifier: "viewMeme", sender: (indexPath.row, appDelegate.memes.remove(at: indexPath.row)))
+    }
+}
+
+// MARK: - Navigation
+extension SentMemesCollectionViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "viewMeme" {
+            if let viewMemeController = segue.destination as? ViewMemeViewController {
+                if let meme = sender as? (Int, Meme) {
+                    viewMemeController.memeIndex = meme.0
+                    viewMemeController.meme = meme.1
+                }
+            }
+        }
     }
 }
